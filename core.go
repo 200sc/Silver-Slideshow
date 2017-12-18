94c50d7166cb39c1eb60ce2875bbe9e2721bbe6d
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-18 16:17:00 -0600

    slide pictures

--- core.go
+++ core.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/png"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/200sc/silver-slideshow/internal"
 	"github.com/oakmound/oak"
+	"github.com/oakmound/oak/dlog"
+	"github.com/oakmound/oak/event"
 	"github.com/oakmound/oak/examples/slide/show"
 	"github.com/oakmound/oak/examples/slide/show/static"
 	"github.com/oakmound/oak/render"
@@ -96,6 +102,25 @@ func main() {
 	for i, s := range sslides {
 		slides[i] = s
 	}
+
+	shotIndex := 0
+
+	oak.AddCommand("allShots", func([]string) {
+		for i := 0; i <= len(slides); i++ {
+			rgba := oak.ScreenShot()
+			f, err := os.Create("slides/slide" + strconv.Itoa(shotIndex) + ".png")
+			if err != nil {
+				dlog.Error(err)
+				return
+			}
+			png.Encode(f, rgba)
+			dlog.ErrorCheck(f.Close())
+			shotIndex++
+			time.Sleep(1000 * time.Millisecond)
+			event.Trigger("KeyUpRightArrow", nil)
+			time.Sleep(1000 * time.Millisecond)
+		}
+	})
 
 	show.AddNumberShortcuts(len(slides))
 	show.Start(slides...)
